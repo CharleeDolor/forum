e57b7a1417f7ac27064610e8745d680c60e73d63
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-05-11 16:40:01 +0800

    improved error messages login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,31 +2,52 @@
 
 
 @section('content')
-<style>
-    .inputType{
-        height: 50px;
-        width: 300px;
-    }
-</style>
-    <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
+    <style>
+        .inputType {
+            height: 50px;
+            width: 300px;
+        }
+
+        .error-container {
+            left: 0;
+            right: 0;
+
+            margin-left: auto;
+            margin-right: auto;
+
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+    </style>
+
+    <div class="error-container w-25 m-3">
         @if (\Session::has('success'))
-            <div class="alert alert-success fixed-top" id="success-alert">
+            <div class="alert alert-success alert-dismissible fade show m-3" id="success-alert">
                 <button type="button" class="close" data-dismiss="alert">x</button>
                 <strong> {!! \Session::get('success') !!} </strong> Login using your email and password
             </div>
         @endif
 
         @if ($message = Session::get('error'))
-            <div class="alert alert-danger alert-block">
+            <div class="alert alert-danger alert-dismissible fade show alert-block m-3">
                 <button type="button" class="close" data-dismiss="alert">x</button>
-                <strong>{{ $message }}</strong>
+                <span class="p-4"><strong>{{ $message }}</strong></span>
             </div>
         @endif
-
-        <?php
-        
-        
-        ?>
+        @if ($errors->any())
+            {!! implode(
+                '',
+                $errors->all(
+                    "<div class='alert alert-danger alert-dismissible fade show alert-block m-3'>
+                                        <span class='p-4'><strong>:message</strong></span>
+                                        <button type='button' class='close' data-dismiss='alert'>x</button>
+                                    </div>",
+                ),
+            ) !!}
+        @endif
+    </div>
+    <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
 
         <div class="p-2">
             <form action="{{ route('auth.doLogin') }}" method="POST">
@@ -35,9 +56,11 @@
                         <h1>Login</h1>
                     </div>
                     @csrf
-                    <input type="email" class="inputType" name="email" id="email" placeholder="Email address">
+                    <input type="email" class="inputType" name="email" id="email" placeholder="Email address"
+                        value="{{ old('email') }}">
                     <br>
-                    <input type="password" class="inputType" name="password" id="password" placeholder="password">
+                    <input type="password" class="inputType" name="password" id="password" placeholder="password"
+                        value="{{ old('password') }}">
                     <br>
                     <input type="submit" value="Login" class="btn btn-success">
                     <a href="{{ route('user.create') }}">Create an account</a>
