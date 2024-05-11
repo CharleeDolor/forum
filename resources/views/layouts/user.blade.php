23c8ac22152e17e8ff6e20e2596279b1ce7ecde5
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-05-11 15:52:06 +0800

    improved error message display in create user

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -7,8 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body{
+        body {
             background-color: rgb(240, 240, 240);
+        }
+
+        .error {
+            color: red;
         }
     </style>
     <title>User</title>
@@ -19,7 +23,7 @@
     <div class="container">
         @yield('content')
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.user')
 
 @section('content')
-    <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
 
+    <div class="d-flex align-items-center justify-content-center min-vh-100 flex-column">
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                {!! implode(
+                    '',
+                    $errors->all("
+                                    <p><strong>:message</strong>
+                                "),
+                ) !!}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
         <form action="{{ route('user.store') }}" method="POST">
             @csrf
             <div class="d-flex align-items-center justify-content-center flex-column bg-white rounded p-lg-5">
@@ -11,48 +24,47 @@
 
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="text" name="name" id="name" class="form-control" required placeholder="Name">
+                    <input type="text" name="name" id="name" class="form-control" required placeholder="Name"
+                        value="{{ old('name') }}">
                 </div>
 
                 <div class="form-group">
                     <label for="email">Email address</label>
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                    
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                        id="email" aria-describedby="emailHelp" placeholder="Enter email" 
+                        value="{{ old('email') }}">
                 </div>
-
-
 
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                        name="password" required autocomplete="current-password">
+                        name="password" required autocomplete="current-password" 
+                        value="{{ old('password') }}">
 
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                </div>
+                <div class="d-flex justify-content-center flex-column">
+                    <div><small>Atleast 6 characters long</small></div>
+                    <div><small>Atleast one lower case letter</small></div>
+                    <div><small>Atleast one upper case letter</small></div>
+                    <div><small>Atleast one special character</small></div>
+                    <div><small>Atleast one digit</small></div>
                 </div>
 
                 <div class="form-group">
                     <label for="password">Confirm Password</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                        name="password_confirmation" required autocomplete="current-password">
+                        name="password_confirmation" required autocomplete="current-password" 
+                        value="{{ old('password_confirmation') }}">
                 </div>
-                    
-                
-                <hr class="w-100 bg-dark"/>
+
+
+                <hr class="w-100 bg-dark" />
                 <div class="form-group">
                     <input type="submit" value="Create Account" class="btn btn-success">
                 </div>
 
                 <div class="form-group">
-                    <a href="{{route('user.login')}}">Already have an account?</a>
+                    <a href="{{ route('user.login') }}">Already have an account?</a>
                 </div>
             </div>
 
